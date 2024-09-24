71a38115c3e172c3c71a70a3f2ca5ad4d15c74ad
Sheeb <thingisss89@gmail.com>
2024-09-24 15:07:01 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameWindowSettings = GameWindowSettings.Default;
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Tetris 2D",
+                Flags = ContextFlags.ForwardCompatible
+
+            };
+
+            using (TetrisGame game = new TetrisGame(gameWindowSettings, nativeWindowSettings))
+            {
+                game.Run();
+
+            }
+
+
+
+        }
+
+    }
+}
+
+
---
+++ TetrisGame.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Tetris
+{
+    internal class TetrisGame : GameWindow
+    {
+        public TetrisGame(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f); //Negro
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            //Logia de movimiento y cntrol del juego
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            //Codigo para dibujar cosas en el tablero
+
+            SwapBuffers();
+        }
+    }
+
+}
+
